17fb7fd2bcfd0e737aabd6322e964b225b3a5424
Kevin Wijnen <58944808+kevin-wijnen@users.noreply.github.com>
2024-03-30 22:34:37 +0100

    refactor: easier .bat script by @imLinguin

--- dummy-service/comet-dummy-service.bat
+++ dummy-service/comet-dummy-service.bat
@@ -1,1 +1,8 @@
-sc create GalaxyCommunication binpath=C:\\ProgramData\\GOG.com\\Galaxy\\redists\\GalaxyCommunication.exe
+@echo off
+
+SET currdir=%~dp0
+SET targetdir=C:\ProgramData\GOG.com\Galaxy\redists
+
+sc create GalaxyCommunication binpath=%targetdir%\GalaxyCommunication.exe
+if not exist "%targetdir%" mkdir %targetdir% 
+xcopy /y /q %currdir%GalaxyCommunication.exe %targetdir%
