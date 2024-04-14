95753236846cbd0980e679c4dbe73fb5908a6956
Paweł Lidwin <lidwinpawel@gmail.com>
2024-04-14 22:08:36 +0200

    build: respect PROTOC env var

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
 use protobuf_codegen::Codegen;
+use std::path::PathBuf;
 
 fn main() {
+    println!("cargo:rerun-if-changed=proto/galaxy.protocols.communication_service.proto");
+    println!("cargo:rerun-if-env-changed=PROTOC");
+    let vendored_protoc = protoc_bin_vendored::protoc_bin_path().unwrap();
+    let protoc_path = match std::env::var("PROTOC") {
+        Ok(protoc) => PathBuf::from(&protoc),
+        Err(_) => vendored_protoc,
+    };
     Codegen::new()
         .protoc()
-        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
+        .protoc_path(protoc_path.as_path())
         .includes(["proto"])
         .input("proto/gog.protocols.pb.proto")
         .input("proto/galaxy.protocols.webbroker_service.proto")
