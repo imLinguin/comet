f1fccb8d4dcb88d001c9a13de874d495c70fcc26
Paweł Lidwin <lidwinpawel@gmail.com>
2024-07-11 11:54:27 +0200

    ci: support drafting releases on tag push

---
+++ ci/package-build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+target=$1
+params=''
+
+if [[ -z $target ]]; then
+    echo "Target not provided"
+    exit 1
+fi
+
+if [ "$RUNNER_OS" = "Linux" ]; then
+    params='docs/steamdeck'
+fi
+
+cp "target/$target/release/comet*" .
+
+if [ "$RUNNER_OS" = "Windows" ]; then
+    7z a "comet-$target.zip" comet.exe dummy-service/*{.exe,.md} 
+    echo "archive_name=comet-$target.zip" >> "$GITHUB_OUTPUT"
+else 
+    tar -czvf "comet-$target.tar.gz" comet dummy-service/*{.exe,.md} $params
+    echo "archive_name=comet-$target.tar.gz" >> "$GITHUB_OUTPUT"
+fi
+
