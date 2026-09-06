aa5f5f6992645d938af60a33db85c1a0d3bb9f28
Paweł Lidwin <lidwinpawel@gmail.com>
2026-09-06 17:23:24 +0200

    debug: add arbitrary sleep to responses

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -9,7 +9,7 @@ mod webbroker;
 
 use crate::{constants::TokenStorage, proto::common_utils::ProtoPayload};
 use error::*;
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use crate::api::gog;
 use crate::api::notification_pusher::PusherEvent;
@@ -72,6 +72,7 @@ pub async fn entry_point(
                             tokio::spawn(async move {
                                 match handle_message(&context, user, &reqwest_client, payload).await {
                                     Ok(res) => {
+                                        tokio::time::sleep(Duration::from_secs(2)).await;
                                         if let Err(err) = context.socket_write_mut().await.write_all(&res).await {
                                             error!("Failed to write response {err}");
                                         }
